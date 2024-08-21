8615a7fbb7ca3ed356bacd1fd1a39a6f9bb59d34
Christian Winkelmann <christian.winkelmann@gmail.com>
2024-08-21 12:19:39 +0200

    running the code with much more iguana data

--- notebooks/run.py
+++ notebooks/run.py
@@ -3,13 +3,17 @@ import pandas as pd
 
 import pandas
 
+# class_dict = {
+#     1: 'Hartebeest',
+#     2: 'Buffalo',
+#     3: 'Kob',
+#     4: 'Warthog',
+#     5: 'Waterbuck',
+#     6: 'Elephant',
+# }
+
 class_dict = {
-    1: 'Hartebeest',
-    2: 'Buffalo',
-    3: 'Kob',
-    4: 'Warthog',
-    5: 'Waterbuck',
-    6: 'Elephant',
+    1: 'iguana',
 }
 
 def _set_labels_species(cls_dict: dict, df: pandas.DataFrame) -> None:
@@ -27,12 +31,15 @@ if __name__ == "__main__":
     # rename the columns
 
     # read the csv file
-    df_train_patches = pd.read_csv("../data/val_patches.csv")
+    # df_val_patches = pd.read_csv("../data_iguana/val_patches/gt.csv")
+    #
+    # _set_labels_species(class_dict, df_val_patches)
+    #
+    # df_val_patches.to_csv("../data_iguana/val_patches/gt.csv", index=False)
 
-    # rename the columns
-    df_train_patches
 
+    df_train_patches = pd.read_csv("../data_iguana/train_patches/gt.csv")
 
     _set_labels_species(class_dict, df_train_patches)
 
-    df_train_patches.to_csv("../data/val_patches.csv", index=False)
+    df_train_patches.to_csv("../data_iguana/train_patches/gt.csv", index=False)
